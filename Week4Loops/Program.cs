8920060bb4d9ca7262870299aad930b69f262bd9
daria-koivisto <daria.koivisto@tktk.ee>
2023-11-13 10:59:07 +0200

    week 4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"{i+1}.I will not skip Zoom classes.");
+//}
+
+Random rnd  = new Random();
+
+int myRandom;
+
+for (int i = 0; i < 3; i++)
+{
+    myRandom = rnd.Next(1, 11);
+    Console.WriteLine($"{i+1}. {myRandom}");
+
+}
